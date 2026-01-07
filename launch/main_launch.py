8b19b4a97ca7a6d228f85f45ac735995d6479cdc
Alex Romero <alex.romero@cdei.upc.edu@montperdut-ubuntu.cdei.upc.edu>
2026-01-07 11:56:14 +0100

    realsense and thermal to 15 fps

--- launch/main_launch.py
+++ launch/main_launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     debug_launch_arg = DeclareLaunchArgument("debug", default_value='false')
     focus_launch_arg = DeclareLaunchArgument("focus", default_value='70')
     yolo_model_launch_arg = DeclareLaunchArgument("yolo_model", default_value='General_Plants_19_06-seg.pt')
-    rgbd_resolution_launch_arg = DeclareLaunchArgument("rgbd_resolution", default_value='640,480,30')
+    rgbd_resolution_launch_arg = DeclareLaunchArgument("rgbd_resolution", default_value='640,480,15')
     homography_file_launch_arg = DeclareLaunchArgument("homography_file", default_value='1280')
     name_class_id_launch_arg = DeclareLaunchArgument("name_class", default_value='plant')
     number_class_id_launch_arg = DeclareLaunchArgument("number_class", default_value='0')
